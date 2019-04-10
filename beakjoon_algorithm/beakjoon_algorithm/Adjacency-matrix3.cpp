40375f80908f7f9b3f425646b65503934cc87b8c
kgh940525 <kgh940525@gmail.com>
2019-04-10 21:43:47 +0900

    Adjacency-matrix Example3 - vector<pair<int,int>> 형식으로 넣는 예제

---
+++ beakjoon_algorithm/beakjoon_algorithm/Adjacency-matrix3.cpp
@@ -0,0 +1,38 @@
+//
+//  Adjacency-matrix3.cpp
+//  beakjoon_algorithm
+//
+//  Created by kgh on 10/04/2019.
+//  Copyright © 2019 kgh. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(void){
+    int n,m;
+    vector<pair<int,int>> a[10];
+    
+    cin >> n >> m;
+    
+    for(int i=0; i<m; i++){
+        int u,v,w;      // u-> 정점1
+                        // v-> 정점2
+                        // w-> 가중치1
+        // u값은 출발점 vertex1
+        // v값도 출발점 vectex2
+        // w값은 weight
+        
+        //****실수 할 수 있음 *****
+        // 벡터에 넣는 순서([u]->[v]->[w])
+        // [v]->[u]->[w]
+        
+        cin >> u >> v;
+        a[u].push_back(make_pair(v,w));
+        a[v].push_back(make_pair(u,w));
+        
+    }
+    
+    
+}
