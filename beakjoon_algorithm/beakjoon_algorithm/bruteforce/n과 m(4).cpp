5b3285d263098d7f6d5f1977ed5e741c9a29e47a
kgh940525 <kgh940525@gmail.com>
2019-06-15 20:43:33 +0900

    [브루트포스 n과 m(4) 15652]

--- beakjoon_algorithm/beakjoon_algorithm/bruteforce/n과 m(4).cpp
+++ beakjoon_algorithm/beakjoon_algorithm/bruteforce/n과 m(4).cpp
@@ -1,49 +1,46 @@
 //
-//  n과 m(3).cpp
+//  n과 m(4).cpp
 //  beakjoon_algorithm
 //
 //  Created by kgh on 15/06/2019.
 //  Copyright © 2019 kgh. All rights reserved.
 //
-
+// 비 내림차순에 중복선택가능
 #include <stdio.h>
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 int arr[10];
 bool check[10];
-// (1) 다르게 중복선택이 가능하다
-void recursive(int index,int n,int m){
+void recursive(int index,int start,int n,int m){
     
-    if(index==m){
+    if(index == m){
         for(int i=0; i<m; i++){
             cout << arr[i];
             if(i != m-1){
                 cout << ' ';
             }
         }
-        cout << '\n';
+        cout <<'\n';
         return;
     }
     
-    for(int i=1; i<=n; i++){
-        // 중복선택이 가능하므로, 주석 제거 해준다.
+    
+    for(int i=start; i<=n; i++){
         
         arr[index] = i;
         check[index] = true;
-        recursive(index+1,n,m);
+        // i면 중복 허용 , i+1이면 중복허용 X
+        recursive(index+1,i,n,m);
         check[index] = false;
         
     }
-    
-    
 }
+
 int main(void){
     int n,m;
     cin >> n >> m;
     
-    recursive(0,n,m);
+    recursive(0,1,n,m);
     
     return 0;
-    
 }
