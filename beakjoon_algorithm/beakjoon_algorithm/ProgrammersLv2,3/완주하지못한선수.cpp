500f729002fda0ae403116509167b050f6d32941
kgh940525 <kgh940525@gmail.com>
2019-10-08 23:43:49 +0900

    Add Programmers Lv2,3

---
+++ beakjoon_algorithm/beakjoon_algorithm/ProgrammersLv2,3/완주하지못한선수.cpp
@@ -0,0 +1,24 @@
+//
+//  완주하지못한선수.cpp
+//  beakjoon_algorithm
+//
+//  Created by kgh on 08/10/2019.
+//  Copyright © 2019 kgh. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+int main(void){
+    string ans;
+    char a ='b';
+    string str = "";
+    str = str + a;
+    cout << str;
+    
+    
+    return 0;
+}
